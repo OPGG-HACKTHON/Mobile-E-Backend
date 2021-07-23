7f9d6b4d017bdbcd0c1bd7a965451cd779f66333
sujl95 <sujl95@naver.com>
2021-07-23 22:10:21 +0900

    init

---
+++ G-Makers-server/src/test/java/opgg/backend/gmakersserver/GMakersServerApplicationTests.java
@@ -0,0 +1,13 @@
+package opgg.backend.gmakersserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GMakersServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
