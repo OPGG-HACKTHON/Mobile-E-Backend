aea157006ad62eb17e17cc383d9e0c425eb5c35d
sujl95 <sujl95@naver.com>
2021-07-23 22:30:58 +0900

    del G-Makers-server dir move

---
+++ src/main/java/opgg/backend/gmakersserver/GMakersServerApplication.java
@@ -0,0 +1,13 @@
+package opgg.backend.gmakersserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GMakersServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GMakersServerApplication.class, args);
+	}
+
+}
