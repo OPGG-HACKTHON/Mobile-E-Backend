0541529b31e55b2d86370192a4458e6ec819835b
sujl95 <sujl95@naver.com>
2021-07-25 22:45:56 +0900

    Add AWS RDS

--- src/main/java/opgg/backend/gmakersserver/GMakersServerApplication.java
+++ src/main/java/opgg/backend/gmakersserver/GMakersServerApplication.java
@@ -1,7 +1,8 @@
 package opgg.backend.gmakersserver;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
 public class GMakersServerApplication {
