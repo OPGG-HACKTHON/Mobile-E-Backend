c4a8d52a83fa63d12e9d89a0c699f30243bab03a
jihoon <znftm97@naver.com>
2021-07-29 21:11:38 +0900

    Fix jasypt

--- src/main/java/opgg/backend/gmakersserver/jasypt/config/JasyptConfig.java
+++ src/main/java/opgg/backend/gmakersserver/jasypt/config/JasyptConfig.java
@@ -9,17 +9,18 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class JasyptConfig {
 
+	@Bean(name = "jasyptStringEncryptor")
+	public StringEncryptor stringEncryptor() {
 
-	@Bean("jasyptStringEncryptor")
-	public StringEncryptor stringEncryptor(){
 		PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
 		SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-		config.setPassword(System.getProperty("jasypt.encryptor.password"));
-		config.setPoolSize("1");
-		config.setAlgorithm("PBEWithMD5AndDES");
-		config.setStringOutputType("base64");
-		config.setKeyObtentionIterations("1000");
-		config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+		config.setPassword("opgg"); // 암호화할 때 사용하는 키
+		config.setAlgorithm("PBEWithMD5AndDES"); // 암호화 알고리즘
+		config.setKeyObtentionIterations("1000"); // 반복할 해싱 회수
+		config.setPoolSize("1"); // 인스턴스 pool
+		config.setProviderName("SunJCE");
+		config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator"); // salt 생성 클래스
+		config.setStringOutputType("base64"); //인코딩 방식
 		encryptor.setConfig(config);
 		return encryptor;
 	}
--- src/test/java/opgg/backend/gmakersserver/GMakersServerApplicationTests.java
+++ src/test/java/opgg/backend/gmakersserver/GMakersServerApplicationTests.java
@@ -1,6 +1,7 @@
 package opgg.backend.gmakersserver;
 
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,23 +12,9 @@ import com.ulisesbocchio.jasyptspringboot.properties.JasyptEncryptorConfiguratio
 @SpringBootTest
 class GMakersServerApplicationTests {
 
-	@Value("${jasypt.encryptor.password}")
-	private String jasyptEncryptorPassword;
-
 	@Test
-	public void contextLoads() throws Exception {
-		JasyptEncryptorConfigurationProperties properties = new JasyptEncryptorConfigurationProperties();
-		SimpleStringPBEConfig simpleStringPBEConfig = new SimpleStringPBEConfig();
-		simpleStringPBEConfig.setPassword(jasyptEncryptorPassword);
-		simpleStringPBEConfig.setAlgorithm(properties.getAlgorithm());
-		simpleStringPBEConfig.setKeyObtentionIterations(properties.getKeyObtentionIterations());
-		simpleStringPBEConfig.setPoolSize(properties.getPoolSize());
-		simpleStringPBEConfig.setSaltGeneratorClassName(properties.getSaltGeneratorClassname());
-		simpleStringPBEConfig.setIvGeneratorClassName(properties.getIvGeneratorClassname());
-		simpleStringPBEConfig.setStringOutputType(properties.getStringOutputType());
-		PooledPBEStringEncryptor pooledPBEStringEncryptor = new PooledPBEStringEncryptor();
-		pooledPBEStringEncryptor.setConfig(simpleStringPBEConfig);
+	public void contextLoads() {
 	}
-
-
 }
+
+
