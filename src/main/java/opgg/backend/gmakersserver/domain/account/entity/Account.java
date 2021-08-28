77d726c47f14defda50731604617591ae9ba235e
sujl95 <sujl95@naver.com>
2021-08-28 14:07:02 +0900

    [#31] Refactor Account

--- src/main/java/opgg/backend/gmakersserver/domain/account/entity/Account.java
+++ src/main/java/opgg/backend/gmakersserver/domain/account/entity/Account.java
@@ -1,18 +1,32 @@
 package opgg.backend.gmakersserver.domain.account.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.*;
-import opgg.backend.gmakersserver.domain.account.controller.request.SignUpRequest;
-import opgg.backend.gmakersserver.domain.auditing.BaseEntity;
-import opgg.backend.gmakersserver.domain.profile.entity.Profile;
+import static javax.persistence.CascadeType.*;
 
-import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
-import static javax.persistence.CascadeType.ALL;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import opgg.backend.gmakersserver.domain.auditing.BaseEntity;
+import opgg.backend.gmakersserver.domain.profile.entity.Profile;
 
 @Entity
 @Getter
