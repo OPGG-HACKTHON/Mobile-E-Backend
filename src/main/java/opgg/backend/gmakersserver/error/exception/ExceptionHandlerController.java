b4a0958288375fb2c63604733b3ca06c390e519d
sujl95 <sujl95@naver.com>
2021-09-01 00:34:32 +0900

    [#33] Add Logging Log4j2

--- src/main/java/opgg/backend/gmakersserver/error/exception/ExceptionHandlerController.java
+++ src/main/java/opgg/backend/gmakersserver/error/exception/ExceptionHandlerController.java
@@ -61,6 +61,7 @@ public class ExceptionHandlerController {
 	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
 	private ExceptionResponse handleHttpRequestMethodNotSupportedException(
 		HttpRequestMethodNotSupportedException exception) {
+		errorLogging(exception);
 		return ExceptionResponse.of(exception);
 	}
 
